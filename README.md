5f030cda8dd0c5a4b31d07c14bfe5601a1d85e22
Sebastiaan Huber <mail@sphuber.net>
2021-09-23 20:34:27 +0200

    Initial commit of the project
    Contains minimal scaffolding of a Python project including:
    
     * `.pre-commit-config.yaml`: configuration for `pre-commit`
     * `LICENSE.txt`: MIT license
     * `README.md`: long description of the project
     * `pyproject.toml`: project configuration
     * `setup.cfg`: project metadata
     * `setup.py`: shim to allow for editable installs

---
+++ README.md
@@ -0,0 +1,1 @@
+# Biblary
---
+++ setup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Shim setup file that allows editable installs."""
+import setuptools
+
+if __name__ == '__main__':
+    setuptools.setup()
